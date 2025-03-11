1eb9214c1bb67d582f68909cdc5776e1db74b8f7
Dikshashelar <dikshashelar004@gmail.com>
2025-03-11 20:00:02 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Announcement from "../components/Announcement"; // Correct import
+
+function Home() {
+  return (
+    <div>
+      <Announcement />
+      {/* Other homepage content */}
+    </div>
+  );
+}
+
+export default Home;
